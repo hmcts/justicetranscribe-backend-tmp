6493740e64cce40e2453b27f5cc3e805da86b623
Thomas Horrobin <thomas.horrobin"justice.gov.uk>
2025-11-21 15:20:33 +0000

    main

--- src/main/java/uk/gov/hmcts/reform/justicetranscribe/Application.java
+++ src/main/java/uk/gov/hmcts/reform/justicetranscribe/Application.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.demo;
+package uk.gov.hmcts.reform.justicetranscribe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
